99f283ea36a252d6431c67b81a5a1b4fba5a9ec9
Reshma M nair <reshmamnair@localhost.localdomain>
2021-06-09 16:04:08 +0530

    added groovy script for DSL

--- dsl.groovy
+++ dsl.groovy
@@ -1,8 +1,12 @@
 job('New-sample-excelpy-job-from-DSL'){
     description("Sample excelpy fro DSL on ${new Date()}")
     scm {
-        git("https://github.com/maheshmohanmu/excel_manipulation_py", 'master')
-        credentials('maheshgithub')
+        git {
+            remote {
+                url('https://github.com/maheshmohanmu/excel_manipulation_py')
+                credentials('maheshgithub')
+                }
+            }      
     }
     triggers {
         scm('* * * * *')
